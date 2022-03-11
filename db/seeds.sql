eb88dde22787a96a3497002623228fac1100355d
Bradley Boyd <bradley.boyd@gmail.com>
2022-03-11 15:15:06 -0600

    updated seeds and schema.sql

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department(department_name)
+VALUES("Engineering"), ("Sales"), ("Finance"), ("Management"), ("Legal");
+
+INSERT INTO role(title, salary, department_id)
+VALUES("Engineer", 80000, 1), ("Senior Engineer", 120000, 1), ("President", 220000, 4), ("Vice President", 30000, 4); ("Accountant", 30000, 3); ("Account Manager", 30000, 3); ("Legal Team Lead", 30000, 5); ("Lawyer", 30000, 5); ("Salesperson", 30000, 2); ("Sales Team Lead", 30000, 2); ("Intern", 30000, 2);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Walter', 'White', 1, 2), ('Jesse', 'Pinkman', 1, null), ('Hank', 'Schrader', 1, 2), ('Saul', 'Goodman', 2, 2), ('Skyler', 'White', 4, null); ('Todd', 'Alquist', 4, null); ('Steven', 'Gomez', 4, null); ('Gus', 'Fring', 4, null); ('Mike', 'Ehrmantraut', 4, null); ('Tuco', 'Salamanca', 4, null); ('Huell', 'Babineaux', 4, null);
+
+-- engineer = role_id 1
+-- senior engineer = role_id 2
+-- president = role_id 3
+-- vice president = role_id 4
+-- accountant = role_id 5
+-- account manager = role_id 6
+-- legal team lead = role_id 7
+-- lawyer = role_id 8
+-- salesperson = role_id 9
+-- sales team lead = role_id 10
+-- intern = role_id 11
