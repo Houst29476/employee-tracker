ba55ca20463f992e269956ce98fa80dbfd1eb393
Bradley Boyd <bradley.boyd@gmail.com>
2022-03-11 15:43:39 -0600

    updated seeds and schema.sql files

--- db/schema.sql
+++ db/schema.sql
@@ -3,18 +3,19 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL,
+    description TEXT
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
-    department_id INT
+    department_id INT,
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
