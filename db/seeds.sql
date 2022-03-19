9a1b6a58504a9f3eb36a7983291ab668d23db87e
Bradley Boyd <bradley.boyd@gmail.com>
2022-03-19 13:39:24 -0500

    updated Seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,41 +3,41 @@ USE employee_tracker;
 INSERT INTO
   departments (departments_name)
 VALUES
-  ('Engineering'), 
-  ('Sales'), 
-  ('Finance'), 
-  ('Management'), 
-  ('Legal');
+  ("Engineering"), 
+  ("Sales"), 
+  ("Finance"), 
+  ("Management"), 
+  ("Legal");
 
 INSERT INTO
   roles (title, salary, departments_id)
 VALUES
-  ('Engineer', '80000', '1'), 
-  ('Senior Engineer', '120000', '1'), 
-  ('President', '220000', '4'), 
-  ('Vice President', '200000', '4'), 
-  ('Accountant', '50000', '3'), 
-  ('Account Manager', '70000', '3'), 
-  ('Legal Team Lead', '130000', '5'), 
-  ('Lawyer', '120000', '5'), 
-  ('Salesperson', '50000', '2'), 
-  ('Sales Team Lead', '60000', '2'), 
-  ('Intern', '30000', '2');
+  ("Engineer", 80000, 1), 
+  ("Senior Engineer", 120000, 1), 
+  ("President", 220000, 4), 
+  ("Vice President", 200000, 4), 
+  ("Accountant", 50000, 3), 
+  ("Account Manager", 70000, 3), 
+  ("Legal Team Lead", 130000, 5), 
+  ("Lawyer", 120000, 5), 
+  ("Salesperson", 50000, 2), 
+  ("Sales Team Lead", 60000, 2), 
+  ("Intern", 30000, 2);
 
 INSERT INTO
   employees (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Walter', 'White', '1', null), 
-  ('Jesse', 'Pinkman', '2', '2'), 
-  ('Hank', 'Schrader', '8', null), 
-  ('Saul', 'Goodman', '7', null), 
-  ('Skyler', 'White', '3', '3'), 
-  ('Todd', 'Alquist', '6', null), 
-  ('Steven', 'Gomez', '5', null), 
-  ('Gus', 'Fring', '4', '4'), 
-  ('Mike', 'Ehrmantraut', '10', null), 
-  ('Tuco', 'Salamanca', '11', null), 
-  ('Huell', 'Babineaux', '9', null);
+  ("Walter", "White", 1, null), 
+  ("Jesse", "Pinkman", 2, 2), 
+  ("Hank", "Schrader", 8, null), 
+  ("Saul", "Goodman", 7, null), 
+  ("Skyler", "White", 3, 3), 
+  ("Todd", "Alquist", 6, null), 
+  ("Steven", "Gomez", 5, null), 
+  ("Gus", "Fring", 4, 4), 
+  ("Mike", "Ehrmantraut", 10, null), 
+  ("Tuco", "Salamanca", 11, null), 
+  ("Huell", "Babineaux", 9, null);
 
 SELECT * FROM employees;
 
